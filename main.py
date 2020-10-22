dfc674df76f543aeba41b36402f711684e7e55ae
William O'Brien <wobrien2@uw.edu>
2020-10-22 16:44:41 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -17,12 +17,21 @@ def get_fact():
     return facts[0].getText()
 
 
+def post_text(fact):
+    url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    myobj = {'input_text': fact}
+    response = requests.post(url, data=myobj)
+    return response.url
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    body = post_text(fact)
+
+    return Response(response=body, mimetype='text/plain')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
